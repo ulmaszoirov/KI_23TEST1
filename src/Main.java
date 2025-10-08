d5ea04a0c9426843e04c4111e57b42b11ebdc6a8
unknown <Nordic@NIU-0020.uni.loc>
2025-10-08 16:15:52 +0500

    man boshqa gruhlarni ham qo'shdim2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Salom KI-23 talabalari");
+        System.out.println("Salom KI-24 talabalari");
+        System.out.println("Salom KI-22 talabalari");
+        System.out.println("Salom KI-21 talabalari");
     }
 }
